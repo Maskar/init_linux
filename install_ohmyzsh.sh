d6a24c80967e4a191073fbc07f3a03088eb064b1
Maskar <1695584+Maskar@users.noreply.github.com>
2021-04-17 21:15:10 -0500

    update

--- install_ohmyzsh.sh
+++ install_ohmyzsh.sh
@@ -13,9 +13,18 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo "Backup '.zshrc' as '.zshrc.bak'..."
+cp $HOME/.zshrc $HOME/.zshrc.bak
+
 echo "Set theme powerlevel10k..."
+#echo "$(cat $HOME/init_linux/zshrc.top)"$'\n'"$(cat $HOME/.zshrc)" > $HOME/.zshrc
+cp $HOME/init_linux/zshrc.top $HOME/.zshrc
+cat $HOME/.zshrc.bak >> $HOME/.zshrc
+cat $HOME/init_linux/zshrc.end >> $HOME/.zshrc
 sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/' $HOME/.zshrc
+
 echo "Set plugins..."
 sed -i 's/plugins=(git)/plugins=(safe-paste git zsh-completions zsh-autosuggestions zsh-syntax-highlighting)/' $HOME/.zshrc
+
 echo "Copy settings..."
 cp $HOME/init_linux/p10k.zsh $HOME/.p10k.zsh

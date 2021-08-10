d99f9cf34875fe748535d5529c7adac154d03589
Maskar <1695584+Maskar@users.noreply.github.com>
2021-08-09 19:24:55 -0500

    update IPs and autoswitch_virtualenv

--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoswitch_virtualenv
 
 echo "Backup '.zshrc' as '.zshrc.bak'..."
 cp $HOME/.zshrc $HOME/.zshrc.bak
